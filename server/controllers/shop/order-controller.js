bb75a89c990bc07a95593fa18547df05baf53e95
Anany Khare <ananykhare04@gmail.com>
2025-11-08 00:56:08 +0530

    Save my local changes before pulling

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -35,8 +35,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "https://weaver-seven.vercel.app/shop/paypal-return",
-        cancel_url: "https://weaver-seven.vercel.app/shop/paypal-cancel",
+        return_url: "https://weaver-shop.vercel.app/shop/paypal-return",
+        cancel_url: "https://weaver-shop.vercel.app/shop/paypal-cancel",
       },
       transactions: [
         {
